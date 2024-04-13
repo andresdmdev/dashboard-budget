352adf1939159bd5cc7de49716e4af05d72709eb
Andres Marquez <andresdmf55@gmail.com>
2024-04-12 23:10:38 -0500

    creater service and handle user logic

---
+++ DTOs/UserDashboardDTO.cs
@@ -0,0 +1,25 @@
+﻿using DomainModel.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dashboard_budget.DTOs
+{
+    //private class UserDashboardDTO2
+    //{
+    //    public string? Name { get; set; }
+    //    public string Email { get; set; }
+    //    public string Password { get; set; }
+    //    public string? MobilePhone { get; set; }
+    //    public string? Address { get; set; }
+    //    public string? City { get; set; }
+    //    public string? Country { get; set; }
+    //    public UserStatus? Status { get; set; }
+    //    public int Age { get; set; }
+    //    public string? CreatedBy { get; set; }
+    //    public string? UpdatedBy { get; set; }
+    //    public string? DeletedBy { get; set; }
+    //}
+}
--- HttpTriggers/UserDashboardTrigger/CreateUser.cs
+++ HttpTriggers/UserDashboardTrigger/CreateUser.cs
@@ -9,45 +9,63 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using DomainModel.Operation;
 using DomainModel.User;
+using System.Diagnostics.Metrics;
+using System.Net;
+using dashboard_budget.DTOs;
+using ApplicationServices.UserServices;
 
 namespace dashboard_budget.HttpTriggers.UserDashboardTrigger
 {
     public class CreateUser
     {
         private readonly ILogger<CreateUser> _logger;
-        private readonly IUserRepository _userRepository;
+        private readonly IUserService userService;
 
-        public CreateUser(ILogger<CreateUser> logger, IUserRepository testRepository)
+        public CreateUser(ILogger<CreateUser> logger, IUserService service)
         {
             _logger = logger;
-            _userRepository = testRepository;
+            userService = service;
         }
 
         [Function("CreateUser")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req, [FromBody] UserDashboardDTO body)
         {
+            DateTime dateTime = DateTime.UtcNow;
+
+            _logger.LogInformation("CreateUser Function HTTP Trigger | Start");
 
             UserDashboard userDashboard = new UserDashboard()
             {
-                Name = "Andres",
-                Email = "hola@gmail.com",
-                Password = "password",
-                Status = UserStatus.Active
+                Name = body.Name,
+                Email = body.Email,
+                Password = body.Password,
+                Status = UserStatus.Active,
+                MobilePhone = body.MobilePhone,
+                Address = body.Address,
+                City = body.City,
+                Country = body.Country,
+                Age =  Convert.ToInt32(body.Age),
+                CreatedBy = body.CreatedBy
             };
 
-            int id = 4;
-            // Funcionando
-            //UserDashboard userCreated = _userRepository.Create(userDashboard);
+            UserDashboard user = userService.CreateUserDashboard(userDashboard);
 
-            UserDashboard userCreated = _userRepository.Get(id);
-
-
-            //List<UserDashboard> user = dBContext.UserDashboard.ToList();
-
-            Thread.Sleep(2000);
-
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            _logger.LogInformation($"CreateUser Function HTTP Trigger | End | ExecutionTime: {dateTime.ToString()}");
+            return new OkObjectResult(user);
+        }
+        public record UserDashboardDTO
+        {
+            public string? Name { get; set; }
+            public string? Email { get; set; }
+            public string? Password { get; set; }
+            public string? MobilePhone { get; set; }
+            public string? Address { get; set; }
+            public string? City { get; set; }
+            public string? Country { get; set; }
+            public string? Age { get; set; }
+            public string? CreatedBy { get; set; }
+            public string? UpdatedBy { get; set; }
+            public string? DeletedBy { get; set; }
         }
     }
 }
